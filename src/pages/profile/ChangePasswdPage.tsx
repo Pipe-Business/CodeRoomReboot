09fce2091f205dcb2e7ff0af036c09e05830b804
hyerimRoh <mtxpvksh@gmail.com>
2024-05-07 20:19:55 +0900

    feat. 비밀번호 재설정 구현

--- src/pages/profile/ChangePasswdPage.tsx
+++ src/pages/profile/ChangePasswdPage.tsx
@@ -1,18 +1,20 @@
-import React, { FC , useCallback, useRef, useState, useEffect } from 'react';
+import React, { FC, useCallback, useRef, useState, useEffect } from 'react';
 import useInput from '../../hooks/useInput';
 import styled from '@emotion/styled';
 import MainLayout from '../../layout/MainLayout';
 import { CenterBox } from '../main/styles';
 import { Link, useNavigate } from 'react-router-dom';
-import { Dialog, DialogContent, DialogTitle, IconButton ,Card, TextField, Box, Button, Divider} from '@mui/material';
+import { Dialog, DialogContent, DialogTitle, IconButton, Card, TextField, Box, Button, Divider } from '@mui/material';
 import { EMAIL_EXP } from '../../constants/define';
 import { toast } from 'react-toastify';
 import { ColorButton } from './styles';
 import { MarginHorizontal } from '../../components/styles';
+import { apiClient } from '../../api/ApiClient';
+import { supabase } from '../../api/ApiClient';
 
 
 interface Props {
-	children?: React.ReactNode;
+    children?: React.ReactNode;
 }
 const CardSection = styled.div`
   display: flex;
@@ -27,84 +29,75 @@ const CenterWrapper = styled.div`
   align-items: center;
 `;
 
+// 비밀번호 변경
+const ResetPasswdPage: FC<Props> = () => {
 
-const ChangePasswdPage: FC<Props> = () => {
+    const [showResetForm, setShowResetForm] = useState<boolean>(false);
+    useEffect(() => {
+        supabase.auth.onAuthStateChange(async (event, session) => {
+            if (event == "PASSWORD_RECOVERY") {
+                setShowResetForm(true);
+            }
+        })
+    }, [])
 
 
-const [inputEmail, onChangeEmail, setInputEmail] = useInput('');
-const inputEmailRef = useRef<HTMLInputElement | null>(null);
-const inputPwdRef = useRef<HTMLInputElement | null>(null);
-const [inputPwd, onChangePwd, setInputPwd] = useInput('');
-const navigate = useNavigate();
-const onSubmitLoginForm = useCallback(async (e: any) => {
-e.preventDefault();
-if (inputEmail === '' || inputEmail.trim() === '') {
-    toast.error('이메일을 입력해주세요');
-    inputEmailRef.current?.focus();
-    return;
-}
-if (!EMAIL_EXP.test(inputEmail)) {
-    toast.error('이메일 양식에 맞게 입력해주세요');
-    inputEmailRef.current?.focus();
-    return;
+    const inputPwdRef = useRef<HTMLInputElement | null>(null);
+    const [inputPwd, onChangePwd, setInputPwd] = useInput('');
+    const navigate = useNavigate();
+    const onSubmitLoginForm = useCallback(async (e: any) => {
+        e.preventDefault();
 
-}
-if (inputPwd === '' || inputPwd.trim() === '') {
-    toast.error('비밀번호를 입력해주세요');
-    inputPwdRef.current?.focus();
-    return;
-}
+        if (inputPwd === '' || inputPwd.trim() === '') {
+            toast.error('비밀번호를 입력해주세요');
+            inputPwdRef.current?.focus();
+            return;
+        }
 
-setInputEmail('');
-setInputPwd('');
-}, [inputEmail, inputPwd]);
+        setInputPwd('');
+        apiClient.updateUserPassword(inputPwd);
+        await apiClient.signOut();
+        navigate('/');
+
+    }, [inputPwd]);
 
     return (
-   <MainLayout>
+        <MainLayout>
 
-       <MarginHorizontal size={8} style={{marginTop:24,marginBottom:24,}}>
-                    <span style={{ color: '#000000', fontSize: '24px', fontWeight: 'bold' }}>비밀번호 재설정 (준비중..)</span>
-                </MarginHorizontal>
+            <MarginHorizontal size={8} style={{ marginTop: 24, marginBottom: 24, }}>
+                <span style={{ color: '#000000', fontSize: '24px', fontWeight: 'bold' }}>비밀번호 재설정</span>
+            </MarginHorizontal>
 
-    <Card style={{ margin: '8px',borderColor: 'grey', borderWidth:'1px'}} elevation={0} variant='outlined'>
-				<form onSubmit={onSubmitLoginForm} style={{paddingTop:'64px',paddingBottom:'64px',paddingRight:'16px',paddingLeft:'16px'}}>
-					<div>
-						<TextField
-                            sx={{
-                                width: { sm: 300, md: 400 },
-                            }}
-							inputRef={inputEmailRef}
-							value={inputEmail}
-							type={'text'}
-							autoFocus
+            <MarginHorizontal size={8} style={{ marginTop: 16, marginBottom: 16, }}>
+                <span style={{ color: '#000000', fontSize: '16px', fontWeight: 'bold' }}>변경하실 비밀번호를 입력해주세요</span>
+            </MarginHorizontal>
 
-							onChange={onChangeEmail}
-							fullWidth
-							placeholder='이메일 입력' />
-					</div>
-					<Box height={16} />
-					<div>
-						<TextField
-                            sx={{
-                                width: { sm: 300, md: 400 },
-                            }}
-							inputRef={inputPwdRef}
-							value={inputPwd}
-							onChange={onChangePwd}
-							fullWidth
-							type={'password'}
-							placeholder='비밀번호 입력' />
-					</div>
-					<Box height={64} />
-					<ColorButton type={'submit'} sx={{fontSize:'15', width: '100%' }}>로그인</ColorButton>
-						</form>
-			
-				<Box height={16} />
+            {showResetForm &&
+                <Card style={{ margin: '8px', borderColor: 'grey', borderWidth: '1px' }} elevation={0} variant='outlined'>
+                    <form onSubmit={onSubmitLoginForm} style={{ paddingTop: '64px', paddingBottom: '64px', paddingRight: '16px', paddingLeft: '16px' }}>
 
-			
-				<Box height={64} />
-			</Card>
-   </MainLayout>
+                        <div>
+                            <TextField
+                                sx={{
+                                    width: { sm: 300, md: 400 },
+                                }}
+                                inputRef={inputPwdRef}
+                                value={inputPwd}
+                                onChange={onChangePwd}
+                                fullWidth
+                                type={'password'}
+                                placeholder='비밀번호 입력' />
+                        </div>
+                        <Box height={64} />
+                        <ColorButton type={'submit'} sx={{ fontSize: '15', width: '100%' }}>비밀번호 재설정</ColorButton>
+                    </form>
+
+                    <Box height={16} />
+
+
+                    <Box height={64} />
+                </Card>}
+        </MainLayout>
     );
 }
-export default ChangePasswdPage;
+export default ResetPasswdPage;
