aafd2b3d69f7431f74ccf28a4aa9f0ce2f9d5338
hongdroid94 <hongdroid94@gmail.com>
2024-05-20 15:51:13 +0900

    멘토링 페이지 작업

---
+++ src/data/entity/MentoringRequestEntity.ts
@@ -0,0 +1,9 @@
+interface MentoringRequestEntity {
+	id?: number;
+	title: string; // 멘토링 신청 제목 
+	content: string; // 멘토링 신청 내용
+	to_user_token?: string; // 신청 대상 uid
+	from_user_token: string; // 신청자 uid		
+	request_date: Date; // 멘토링 가능일
+	created_at?: Date; // 생성일	
+}
