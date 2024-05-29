af87dd9afad7151fa081c31670b8402515f9512d
hyerimRoh <mtxpvksh@gmail.com>
2024-05-29 16:07:24 +0900

    feat. 코드 판매 알림 구현

--- src/pages/codeInfo/components/CoinPaymentDialog.tsx
+++ src/pages/codeInfo/components/CoinPaymentDialog.tsx
@@ -11,33 +11,22 @@ import { createTodayDate } from '../../../utils/DayJsHelper';
 import { toast } from 'react-toastify';
 import { PurchaseSaleRequestEntity } from '../../../data/entity/PurchaseSaleRequestEntity';
 import ReviewDialog from './ReviewDialog';
+import { useQueryUserLogin } from '../../../hooks/fetcher/UserFetcher';
+import { NotificationType } from '../../../enums/NotificationType';
+import { NotificationEntity } from '../../../data/entity/NotificationEntity';
 
 const CashPaymentDialog = () => {
 	const { id } = useParams();
 	const queryClient = useQueryClient();
-	const [userLogin, setUser] = useState<User | null>(null);
+	const { isLoadingUserLogin, userLogin } = useQueryUserLogin();
 	const navigate = useNavigate();
 	
 
 
 	const { isLoading: isCashDataLoading, data: cashData } = useQuery({
 		queryKey: [REACT_QUERY_KEY.cash],
-		queryFn: () => apiClient.getUserTotalCash(userLogin!.id),
+		queryFn: () => apiClient.getUserTotalCash(userLogin!.userToken!),
 	});
-
-	useEffect(() => {
-		const getSession = async () => {
-			const { data, error } = await supabase.auth.getSession()
-			if (error) {
-				console.error(error)
-			} else {
-				const { data: { user } } = await supabase.auth.getUser()
-				setUser(user);
-			}
-		}
-		getSession()
-	}, []);
-
 
 	const { data: postData } = useQuery({
 		queryKey: [REACT_QUERY_KEY.code, id],
@@ -50,7 +39,7 @@ const CashPaymentDialog = () => {
 
 			// 유저 캐시 차감 -> 캐시 사용기록 insert
 			const cashHistory: CashHistoryRequestEntity = {
-				user_token: userLogin!.id,
+				user_token: userLogin!.userToken!,
 				cash: postData?.price!,
 				amount: cashData == undefined ? 0 : cashData - postData?.price!,
 				description: "코드 구매",
@@ -65,7 +54,7 @@ const CashPaymentDialog = () => {
 					post_id: postData!.id,
 					price: postData!.price,
 					is_confirmed: false,
-					purchase_user_token: userLogin!.id,
+					purchase_user_token: userLogin!.userToken!,
 					sales_user_token: postData!.userToken,
 					pay_type: "cash"
 				}
@@ -80,11 +69,8 @@ const CashPaymentDialog = () => {
 				await apiClient.updateBuyerCount(postData.buyerCount + 1, postData.id);
 			}
 
-			
-
-            // navigate('/');
-            toast.success('구매가 완료되었습니다.');
-			// todo 구매자에게 구매 알림
+			// navigate('/');
+			toast.success('구매가 완료되었습니다.');
 
 		},
 	});
@@ -94,9 +80,19 @@ const CashPaymentDialog = () => {
 			if (postData && userLogin?.id) {
 				mutate();
 				const todayDate = createTodayDate();
+				//  판매자에게 판매알림
+				const notificationEntity: NotificationEntity ={
+					title : '코드 판매 알림',
+					content: `'${postData?.title}' 게시글이 판매 되었습니다`,
+					from_user_token: '045148b1-77db-4dfc-8d76-e11f7f9a4a10',// todo 변경 필요
+					to_user_token: postData?.userToken!,
+					notification_type: NotificationType.granted,
+				}
+				let notistring=JSON.stringify(notificationEntity);
+				console.log("sdf"+notistring);
+				await apiClient.insertNotification(notificationEntity);
 
-
-				// navigate('/');
+				navigate('/');
 				// toast.success('구매가 완료되었습니다.');
 			
 				// 	const notiEntity: UserNotificationEntity = {
