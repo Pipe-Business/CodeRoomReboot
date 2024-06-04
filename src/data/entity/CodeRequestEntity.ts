13a7f587437a63bb2f4a88c5ee838793a48722bf
hyerimRoh <mtxpvksh@gmail.com>
2024-06-04 17:14:37 +0900

    modify. 코드 게시글 관련 모델에 language 추가

--- src/data/entity/CodeRequestEntity.ts
+++ src/data/entity/CodeRequestEntity.ts
@@ -4,6 +4,7 @@ interface CodeRequestEntity {
     created_at?: number;
     github_repo_url:string; // 깃허브 URL
     cost:number; //가격
+    language:string, // 개발 언어
     github_download_url?: string; // 깃허브 다운로드 url
     seller_github_name: string; // 판매자의 GitHub 사용자명
 	admin_git_repo_url?: string; // 관리자의 포크한 깃헙 URL
