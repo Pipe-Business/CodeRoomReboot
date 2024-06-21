2cab869f5d9716299c9f2f163be7cb97c1760ff8
hyerimRoh <mtxpvksh@gmail.com>
2024-06-21 13:55:10 +0900

    fix. 캐시 충전 시 적립률 변경

--- src/pages/coin/component/PointData.tsx
+++ src/pages/coin/component/PointData.tsx
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 import { Navigate } from 'react-router-dom';
 import PointItem from './PointItem';
 import { useQueryUserLogin } from '../../../hooks/fetcher/UserFetcher';
+import {CACHE_CHARGE_PRD, CACHE_CHARGE_PRD_NAME, REWARD_COIN} from "../../../constants/define";
 
 interface Props {
 	children?: React.ReactNode;
@@ -19,10 +20,10 @@ const PointData: FC<Props> = () => {
 
 	return (
 		<>
-			<PointItem paymentCash={1000} bonusPoint={500} paymentPrice={1000} orderName={'코드룸 캐시 1000+보너스500p'} />
-			<PointItem paymentCash={3000} bonusPoint={1500} paymentPrice={3000} orderName={'코드룸 캐시 3000+보너스1500p'} />
-			<PointItem paymentCash={5000} bonusPoint={2500} paymentPrice={5000} orderName={'코드룸 캐시 5000+보너스2500p'} />
-			<PointItem paymentCash={10000} bonusPoint={5000} paymentPrice={10000} orderName={'코드룸 캐시 10000+보너스5000p'} />
+			<PointItem paymentCash={CACHE_CHARGE_PRD.CACHE_1000} bonusPoint={REWARD_COIN.CACHE_CHARGE_1000_BONUS_COIN} paymentPrice={CACHE_CHARGE_PRD.CACHE_1000} orderName={CACHE_CHARGE_PRD_NAME.CACHE_1000} />
+			<PointItem paymentCash={CACHE_CHARGE_PRD.CACHE_3000} bonusPoint={REWARD_COIN.CACHE_CHARGE_3000_BONUS_COIN} paymentPrice={CACHE_CHARGE_PRD.CACHE_3000} orderName={CACHE_CHARGE_PRD_NAME.CACHE_3000} />
+			<PointItem paymentCash={CACHE_CHARGE_PRD.CACHE_5000} bonusPoint={REWARD_COIN.CACHE_CHARGE_5000_BONUS_COIN} paymentPrice={CACHE_CHARGE_PRD.CACHE_5000} orderName={CACHE_CHARGE_PRD_NAME.CACHE_5000} />
+			<PointItem paymentCash={CACHE_CHARGE_PRD.CACHE_10000} bonusPoint={REWARD_COIN.CACHE_CHARGE_10000_BONUS_COIN} paymentPrice={CACHE_CHARGE_PRD.CACHE_10000} orderName={CACHE_CHARGE_PRD_NAME.CACHE_10000} />
 		</>
 	);
 };
