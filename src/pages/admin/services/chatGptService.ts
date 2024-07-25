5f6b311dab669804977de8e5a2adbfdb26d71a30
hongdroid94 <hongdroid94@gmail.com>
2024-07-25 15:39:32 +0900

    feat. 어드민 코드 검증 시스템 구현완료

---
+++ src/pages/admin/services/chatGptService.ts
@@ -0,0 +1,25 @@
+// services/chatGptService.ts
+import axios from 'axios';
+
+const API_URL = 'https://api.openai.com/v1/chat/completions'; // Replace with the actual endpoint if different
+const API_KEY = process.env.GPT_API_KEY; // Replace with your actual OpenAI API key
+
+export const analyzeCode = async (code: string) => {
+    const response = await axios.post(API_URL, {
+        model: 'gpt-4o-mini',
+        messages: [{ role: 'user', content: `${code} 해당 코드파일의 구현 목적을 1줄로 요약설명해주고, 문제점이나 잠재적인 위험을 진단한 결과를 1줄로 간단히 피드백 해주고, 진단 결과에 대한 코드파일의 안전 위험성을 0~100%로 단답으로 작성해주고, 반드시 한국어로만 답변해줘` }],
+        max_tokens: 1024, // Adjust as needed
+        top_p: 1,
+        temperature: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    }, {
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    console.log('Response:', response.data.choices[0].message.content);
+    return response.data.choices[0].message.content;
+};
---
+++ src/pages/admin/services/githubService.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export const FIREBASE_FUNCTIONS_URL = "https://us-central1-coderoom-c8e3f.cloudfunctions.net/githubapi"
+
+export const fetchRepoFiles = async (owner: string, repo: string, path: string = '') => {
+    try {
+        const response = await axios.get(`${FIREBASE_FUNCTIONS_URL}/repo-files/${owner}/${repo}`);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error('Error fetching file content:', error.response?.data || error.message);
+        } else {
+            console.error('Unknown error:', error);
+        }
+        throw new Error('파일 내용을 가져오는 중 오류가 발생했습니다');
+    }
+};
+
+export const fetchFileContent = async (url: string) => {
+    try {
+        const response = await axios.get(`${FIREBASE_FUNCTIONS_URL}/file-content`, {
+            params: { url }
+        });
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error('Error fetching repo files:', error.response?.data || error.message);
+        } else {
+            console.error('Unknown error:', error);
+        }
+        throw new Error('레포지토리 파일을 가져오는 중 오류가 발생했습니다');
+    }
+};
