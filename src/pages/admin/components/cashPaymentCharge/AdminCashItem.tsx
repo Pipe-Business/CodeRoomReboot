ef71be678c706e7ade0bf64e832271dcebaf9a73
hongdroid94 <hongdroid94@gmail.com>
2024-08-30 12:39:31 +0900

    refactor. 결제 영수증 보기 링크 버튼으로 변경

--- src/pages/admin/components/cashPaymentCharge/AdminCashItem.tsx
+++ src/pages/admin/components/cashPaymentCharge/AdminCashItem.tsx
@@ -1,27 +1,47 @@
-import React, {FC} from 'react';
-import {Divider, ListItem, ListItemText} from '@mui/material';
-import {BootPayPaymentModel} from "../../../../data/entity/BootPayPaymentModel";
+import React, { FC } from 'react';
+import { TableCell, TableRow, Link } from '@mui/material';
+import { BootPayPaymentModel } from "../../../../data/entity/BootPayPaymentModel";
 
 interface Props {
-	children?: React.ReactNode;
 	item: BootPayPaymentModel;
 }
 
-const AdminCashItem: FC<Props> = ({item}) => {
-	return <>
-		<ListItem>
-			<ListItemText>
-				<div style={{display: 'flex'}}>
-					<div style={{width: '20%'}}>{item?.created_at!.toString()}</div>
-					<div style={{width: '25%'}}>{item.order_name}</div>
-					<div style={{width: '10%'}}>{item.price} 원</div>
-					<div style={{width: '10%'}}>{item.method_origin}</div>
-					<div style={{width: '30%'}}>{item.receipt_url}</div>
-				</div>
-			</ListItemText>					{/*	TODO 영수증 링크를 다음줄로 넘기기 구현 필요 */}
-		</ListItem>
-		<Divider/>
-	</>;
+const AdminCashItem: FC<Props> = ({ item }) => {
+	const formatDate = (dateString: string) => {
+		const date = new Date(dateString);
+		return date.toLocaleString('ko-KR', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit',
+			hour: '2-digit',
+			minute: '2-digit'
+		});
+	};
+
+	return (
+		<TableRow>
+			<TableCell>{formatDate(item.created_at!)}</TableCell>
+			<TableCell>{item.order_name}</TableCell>
+			<TableCell align="right">{item.price.toLocaleString()} 원</TableCell>
+			<TableCell>{item.method_origin}</TableCell>
+			<TableCell>
+				<Link
+					href={item.receipt_url}
+					target="_blank"
+					rel="noopener noreferrer"
+					style={{
+						maxWidth: '200px',
+						overflow: 'hidden',
+						textOverflow: 'ellipsis',
+						whiteSpace: 'nowrap',
+						display: 'inline-block'
+					}}
+				>
+					영수증 보기
+				</Link>
+			</TableCell>
+		</TableRow>
+	);
 };
 
 export default AdminCashItem;
